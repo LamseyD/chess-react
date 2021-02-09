5a8badbaab68ae0828a1caf18afb243d049cc058
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-02-09 17:33:58 -0500

    Added Navbar and Navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
+import { Container } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
-        <Navigation/>
+          <Navbar/>
+          <Navigation/>
       </BrowserRouter>
     </div>
   );
