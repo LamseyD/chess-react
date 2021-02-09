e467e051d8604273a8eb3b5dbf8655b83e1c382e
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-02-09 17:32:49 -0500

    Updated Navbar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -4,10 +4,10 @@ export const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+      display: "flex",
+      alignItems: "start",
+      paddingLeft: "2em",
     },
   }));
