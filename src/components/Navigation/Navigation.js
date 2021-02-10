61d6e024d82a881661a9a513faa3a1f502cb3021
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-02-10 18:04:38 -0500

    feat(nav): added link to Result screen

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from '../Home/Home'
 import Auth from '../Auth/Auth'
+import ChessResult from '../ChessResult/ChessResult';
 
 const Navigation = () => {
     return (    
         <Switch>
             <Route exact path="/" component = {Home} />
-            <Route path = "/auth" component = {Auth} /> 
+            <Route path = "/auth" component = {Auth} />
+            <Route path="/results/:username" component={ChessResult}/>
         </Switch>
     )
 
