4466568676053a2f35fdca1ba8f167acd27e3550
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-02-10 19:03:50 -0500

    feat(navbar): updated Navbar for linking and auth

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,50 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
-import { Link } from 'react-router-dom';
-import { useStyles } from './styles';
+import React, {useState, useEffect} from 'react';
+
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import useStyles from './styles';
+
+import decode from 'jwt-decode'
+
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const logout = () => {
+      dispatch({type:'LOGOUT'});
+      history.push('/');
+      setUser(null);
+  }
+  console.log(user);
+
+  const preventDefault = (event) => event.preventDefault();
+
+  //run this when location changes
+  useEffect(() => {
+      const token = user?.token;
+      if (token) {
+          const decodedToken = decode(token);
+          if(decodedToken.exp * 1000 < new Date().getTime())
+              logout();
+      }
+      setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            MyChess
-          </Typography>
+          <div className={classes.title}>
+            <Typography variant="h6" >
+              <Link href="/" onClick={preventDefault} style = {{color: "white"}}>
+                MyChess
+              </Link>
+            </Typography>
+          </div>
           <Button component={Link} to="/auth" variant="contained" color ="primary"> Signin </Button>
         </Toolbar>
       </AppBar>
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
@@ -11,3 +11,5 @@ export const useStyles = makeStyles((theme) => ({
       paddingLeft: "2em",
     },
   }));
+
+export default useStyles;
